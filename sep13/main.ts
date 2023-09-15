fe81573da062e0f8339cbf5bbd260d42b2fc2a95
imran82ali <code.imranali@gmail.com>
2023-09-15 15:39:48 +0500

    first commit

---
+++ sep13/main.ts
@@ -0,0 +1,92 @@
+// let price: number[] = [3000, 2500, 700, 1500];
+
+// let food: Array<string | number> = [
+//   "pizza",
+//   "kabab",
+//   "burger",
+//   "malai boti",
+//   1000,
+// ];
+// console.log(price[2]);
+// price.push(1000)
+// let array4: number[] = new number[2]
+
+// if (-1) {
+//   console.log("truthy");
+// } else {
+//   console.log("falsy");
+// }
+
+// Named Function
+// function Sum(a: number, b: number):number {
+//   return a + b;
+// }
+// Sum(1, 2);
+
+//Anonymous Function
+// let Sum = function (num1: number, num2: number): number {
+//   return num1 + num2;
+// };
+// Sum(1, 2);
+
+// function VoidFunction():void {
+//   console.log("abc");
+//   return
+// }
+
+// let Sum: (a: number, b: number) => number = function (
+//   num1: number,
+//   num2: number
+// ): number {
+//   return num1 + num2;
+// };
+// Sum(1, 2);
+
+// function name(c: () => void) {}
+
+// name(function () {});
+
+// Arrow FUNCTION
+// let Sum: (num1: number) => void = (a: number): void => {};
+// Sum(1);
+
+// function greeter(fn: (message: string) => void) {
+//     fn("hello")
+// }
+
+// function abc(message:string) {
+//     console.log(message);
+// }
+
+// greeter(abc)
+// abc("okasha")
+
+// function abc(name:string,age:number,contactNumber?:number) {
+//     if(contactNumber){
+//         console.log(contactNumber);
+//     }
+//     else{
+//         console.log("no contact number");
+//     }
+// }
+// abc("okasha",19)
+
+// function abc(name: string, age: number, address = "xyz") {
+//   console.log(name, age, address);
+// }
+
+// abc("okasha", 19,"karachi");
+
+// function calculate(operator: string, ...numbers: number[]) {
+// //   console.log(operator, numbers);
+//   let sum = 0
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum = sum + numbers[i]
+//   }
+//   return sum
+// }
+// console.log(calculate("+", 10, 20, 30,23,2,32,3,23,2))
+
+const notFoud:["404","Not Found"] = ["404", "Not Found"];
+
+a.push("asd");
---
+++ sep14/app.ts
@@ -0,0 +1,65 @@
+// let food: string[] = ["pizza", "burger", "tikka","biryani"];
+// let price: Array<number | string> = [1000, 700, 500];
+
+// let names: [string, "Ameen Alam"] = ["okasha", "Ameen Alam"];
+
+// let arr: [string, number, boolean] = ["abc", 1, true];
+
+// let arr: number[] = [];
+// arr.push(1)
+// arr.push(2)
+
+// Named Function
+// function Sum(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+// let result = Sum(12, 12);
+
+// let Sum = function (num1: number, num2: number): number {
+//   return num1 + num2;
+// };
+// console.log(Sum(1, 2));
+
+// let Sum:(num1:number,num2:number)=>number = (num1: number, num2: string): number => {
+//   return num1 + num2;
+// };
+
+// console.log(Sum(1, 2));
+
+// let a = 10;
+// let isTen =
+//if(a === 10)  if-block   else if()  else-if block       else
+//   a === 10 ?   "a is 10" : a > 10 ? "greater than 10" : "a is less than 10";
+// console.log(isTen);
+
+// if (a === 10) {
+//   console.log("a is 10");
+// } else {
+//   console.log("a is not 10");
+// }
+
+// let EvenOdd = (num1: number): string =>
+// num1 % 2 === 0 ? "Even" : "Odd";
+// console.log(EvenOdd(3));
+
+// function greeter(fn: (message: string) => void, gender: string) {
+//   if (gender === "male") {
+//     fn("Hello Man");
+//   } else {
+//     fn("Hello Women");
+//   }
+// }
+
+// function abc(message: string) {
+//   console.log("Message ==> ", message);
+// }
+// greeter(abc, "female");
+
+// function name1(a: number, b: string, c?: boolean) {}
+// name1(1, "sk", true);
+
+
+// function name1(a: number, b: string, c = "asd") {
+//     console.log(c);
+// }
+// name1(1, "sk");
